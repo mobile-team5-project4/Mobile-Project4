e7cb74e23351894366ec80e41520be97f6c8638a
Sethcran <sethcran@gmail.com>
2011-04-06 18:57:22 -0400

    Empty project with README.

---
+++ src/mobile/team5/Project4/Project4.java
@@ -0,0 +1,13 @@
+package mobile.team5.Project4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Project4 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
