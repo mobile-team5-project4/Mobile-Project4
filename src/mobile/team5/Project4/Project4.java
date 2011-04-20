3a19f06df5b241d98945cc77ce734c3aca5dd493
Bryan Pollack <Bryan@Pollacks.net>
2011-04-19 20:35:00 -0400

    Added a start page.

--- src/mobile/team5/Project4/Project4.java
+++ src/mobile/team5/Project4/Project4.java
@@ -2,12 +2,23 @@ package mobile.team5.Project4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Project4 extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(new Game(this));
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Button start = (Button) findViewById(R.id.buttonStart);
+		start.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				setContentView(new Game(Project4.this));
+			}
+		});
+	}
 }
